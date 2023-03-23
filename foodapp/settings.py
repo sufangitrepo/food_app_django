aa000fb68d81a588a1c6e43d38536a2aa1bfd8b8
M Sufyan <97937721+sufangitrepo@users.noreply.github.com>
2023-03-23 14:29:16 +0500

    Update settings.py

--- foodapp/settings.py
+++ foodapp/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%#@i65yxp$4(wptf8-s-0x11$o*#y%ozk(_hlp*unzhq%mr2@w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
