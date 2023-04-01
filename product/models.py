25efa46818daddbd3793fa4c3fe12ece3f1d331d
sufangitrepo <sufyangitrepo1234@gmail.com>
2023-04-02 00:35:41 +0500

    put product api

--- product/models.py
+++ product/models.py
@@ -10,13 +10,22 @@ class Product(models.Model):
     
     def path(instance, filename):
         return ''.join(['images/',f'{instance.product_name}/',filename])
-    
+   
     product_name = models.CharField(unique=True, max_length=100)
+    image = models.ImageField()
     qty = models.IntegerField(default=0)
     category = models.ForeignKey(to=Category, on_delete=models.CASCADE)
-    price = models.DecimalField(max_digits=100, decimal_places=3)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
     description = models.CharField(max_length=200)
-    image = models.ImageField()
+    sale_tax = models.DecimalField(max_digits=6,decimal_places=2)
+    
+    
+
+
+class Charges(models.Model):
+    charges_name = models.CharField(max_length=50, unique=True)
+    amount = models.DecimalField(decimal_places=2,max_digits=6)
+
 
 
     
